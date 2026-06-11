b6b2b9c364d5ce2984bdfab13d3030609fb0f83f
Bindu Harini Gunturi <bgunturi@charlotte.edu>
2026-06-11 15:11:36 -0400

    Updated base class with protected and private members account/routing numbers

--- BankAccUpdate.py
+++ BankAccUpdate.py
@@ -2,10 +2,13 @@ class BankAccount:
 
     bank_t = "Truist Bank"
 
-    def __init__(self, customer_name, current_balance, minimum_balance):
+    def __init__(self, customer_name, current_balance, minimum_balance,account_number, routing_number):
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
+
+        self._account_number = account_number
+        self.__routing_number = routing_number
 
     def deposit(self, d_amount):
         if d_amount > 0:
