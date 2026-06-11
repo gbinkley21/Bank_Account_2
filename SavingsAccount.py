7248348404809ec2fe8587024e7bc1f7fee77747
Gavin Binkley <gavingsb@me.com>
2026-06-11 14:42:42 -0400

    Added the remaining code to SavingsAccount.py and finalized the file

--- SavingsAccount.py
+++ SavingsAccount.py
@@ -1,7 +1,7 @@
-from BankAccount import BankAccount
+from BankAccUpdate import BankAccount
 
 class SavingsAccount(BankAccount):
-    def __init__(self, customer_name, current_balance, minimum_balance, interest rate):
+    def __init__(self, customer_name, current_balance, minimum_balance, interest_rate):
 
         super().__init__(
             customer_name,
@@ -9,7 +9,7 @@ class SavingsAccount(BankAccount):
             minimum_balance
         )
 
-        self.interest_rate = interest rate
+        self.interest_rate = interest_rate
 
     def add_interest(self):
         interest = self.current_balance * self.interest_rate
@@ -18,3 +18,16 @@ class SavingsAccount(BankAccount):
         print(f"Interest added: ${interest:.2f}")
         print(f"New Balance: ${self.current_balance:.2f}")
 
+    if __name__ == "__main__":
+
+        sav1 = SavingsAccount("S1", 1000, 100, 0.05)
+        sav1.print_balance()
+        sav1.add_interest()
+        sav1.print_balance()
+
+        print("\n")
+
+        sav2 = SavingsAccount("S2", 5000, 200, 0.03)
+        sav2.print_balance()
+        sav2.add_interest()
+        sav2.print_balance()
